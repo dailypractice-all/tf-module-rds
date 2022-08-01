36c4abcde3f8e08104e9c78104c0855e93d51bad
gaurav1996b <1996gauravb@gmail.com>
2022-08-01 14:36:19 +0530

    updating terraform file with roboshop project

--- rds.tf
+++ rds.tf
@@ -9,5 +9,5 @@ resource "aws_db_instance" "main" {
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
-  indentifier = local.TAG_PREFIX
+  identifier = local.TAG_PREFIX
 }
