b0acd168eda08c972bcf019c800dcf6ebd40937b
gaurav1996b <1996gauravb@gmail.com>
2022-08-02 18:57:24 +0530

    updating terraform file with roboshop project

--- rds.tf
+++ rds.tf
@@ -1,13 +1,13 @@
 resource "aws_db_instance" "main" {
-  allocated_storage    = 10
-  engine               = var.ENGINE
-  engine_version       = var.ENGINE_VERSION
-  instance_class       = var.RDS_INSTANCE_CLASS
-  username             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
-  password             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
-  parameter_group_name = aws_db_parameter_group.main.name
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.main.name
+  allocated_storage      = 10
+  engine                 = var.ENGINE
+  engine_version         = var.ENGINE_VERSION
+  instance_class         = var.RDS_INSTANCE_CLASS
+  username               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
+  password               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]
+  parameter_group_name   = aws_db_parameter_group.main.name
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
-  identifier = local.TAG_PREFIX
+  identifier             = local.TAG_PREFIX
 }
